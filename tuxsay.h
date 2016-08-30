443cff8c4a8484d4121c1a8593a63a9c99c744d5
lawki <lawki@users.noreply.github.com>
2016-08-30 19:55:19 +0530

    Create tuxsay.h

---
+++ tuxsay.h
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void print_tux()
+{
+	char  line1[]="	    \\         ._____.  ";
+	char  line2[]="	     \\        /     \\";
+	char  line3[]="	      \\      | O _ O |";
+	char  line4[]="               \\     /  \\_/  \\" ;  
+	char  line5[]="                   .' /    \\ `.";
+	char  line6[]="                  / _|       |_\\";
+	char  line7[]="                 (_/ |       |\\_)";
+	char  line8[]="                     \\       /";
+	char  line9[]="                    __\\_>-<_/__";
+	char  line10[]="                      /    \\``";
+	printf("%s\n",line1);
+	printf("%s\n",line2);
+	printf("%s\n",line3);
+	printf("%s\n",line4);
+	printf("%s\n",line5);
+	printf("%s\n",line6);
+	printf("%s\n",line7);
+	printf("%s\n",line8);
+	printf("%s\n",line9);
+	printf("%s\n",line10);
+     
+}
